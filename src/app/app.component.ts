50c229655c712337958b008bddb2632e1de505ea
littlehorseboy <littlehorseboy@gmail.com>
2020-03-25 22:26:46 +0800

    feat: fromEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import '../assets/interval';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-rxjs';
+  @ViewChild('hOne') hOneRef: ElementRef;
+
+  ngAfterViewInit(): void {
+    const mouseMoves = fromEvent(this.hOneRef.nativeElement, 'mousemove');
+
+    const subscription = mouseMoves.subscribe((event: MouseEvent) => {
+      console.log(`Coords: ${event.clientX} X ${event.clientY}`);
+
+      if (event.clientX < 40 && event.clientY < 40) {
+        subscription.unsubscribe();
+      }
+    });
+  }
 }
